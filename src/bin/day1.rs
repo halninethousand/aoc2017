33a30695c928056cc451ef6040d6fe1a36c08a41
unknown <tsvetkovprelude@gmail.com>
2024-10-03 22:30:04 +0300

    day1 :(

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,3 +1,18 @@
 fn main() {
-    let input = include_str!("../../data/day1.txt");
+    let input: Vec<u16> = include_str!("../../data/day1.txt").trim().chars().map(|n| n.to_string().trim().parse::<u16>().unwrap()).collect();
+
+    println!("{input:?}");
+
+    let mut sum = 0;
+
+    for i in 0..input.len() {
+        if i == input.len() - 1 && input[i] == input[0] {
+            sum += input[i];
+        } else if input[i] == input[i+1] {
+            sum += input[i]; 
+        }
+    }
+
+    println!("{sum:?}");
+
 }
