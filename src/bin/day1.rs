2d05b707beb33cbb4373787b417e064bdcc41ad8
unknown <tsvetkovprelude@gmail.com>
2024-10-18 12:43:58 +0300

    back from the dead

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -13,6 +13,20 @@ fn main() {
         }
     }
 
-    println!("{sum:?}");
+    println!("Part1: {sum:?}");
 
+    sum = 0;
+
+    let mut sum = 0;
+    let len = input.len();
+    let half_len = len / 2;
+
+    for i in 0..len {
+        let halfway_index = (i + half_len) % len;
+        if input[i] == input[halfway_index] {
+            sum += input[i];
+        }
+    }
+
+    println!("Part2: {sum:?}");
 }
